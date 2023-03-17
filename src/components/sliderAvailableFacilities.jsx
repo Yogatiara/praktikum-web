6218698aea33cf1843a2dd4d2ebf26654a81d738
dimpram101 <64238889+dimpram101@users.noreply.github.com>
2023-03-17 22:10:39 +0800

    add slider

--- src/components/sliderAvailableFacilities.jsx
+++ src/components/sliderAvailableFacilities.jsx
@@ -1,80 +1,100 @@
 import Chevron from "react-chevron";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const settings = {
+  infinite: true,
+  speed: 500,
+  fade: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+};
 
 const sliderContents = [
-    "./assets/dome.jpg",
-    "./assets/krb.png",
-    "./assets/pantai-manggar.png"
-
-]
-
+  "./assets/dome.jpg",
+  "./assets/krb.png",
+  "./assets/pantai-manggar.png",
+];
 
 const sliderAvailableFacilities = () => {
-    return (
-            
-        <>  
-            <div className="flex   mx-auto  w-[1460px]">
-                <div className="text-3xl  my-auto ">
-                    <button className="px-9 py-0  shadow-xl rounded-full bg-white text-gray-800 hover:bg-white" >
-                        <Chevron direction={'left'} />
-                    </button>
+  return (
+    <>
+      <div className="bg-white  shadow-2xl rounded-3xl mx-auto w-[1200px] h-[600px]">
+        <Slider {...settings} className={"bg-inherit shadow-2xl rounded-3xl"}>
+          <div className="bg-white  shadow-2xl rounded-3xl mx-auto w-[1200px] h-[600px]">
+            <div className="flex p-12 mt-12">
+              <div>
+                <div className="font-archivo text-6xl">
+                  <h2 className="font-extrabold">Pantai</h2>
+                  <h2 className="font-extralight">Manggar.</h2>
+                </div>
+                <div className="pt-10 w-[500px]">
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Cum, molestias! Quam, asperiores debitis eveniet quas nam
+                    aliquid expedita necessitatibus quo sed ea impedit, eum
+                    iusto deserunt quisquam velit temporibus tenetur.
+                  </p>
                 </div>
 
-                <div className="bg-white  shadow-2xl rounded-3xl mx-auto w-[1200px] h-[600px]">
-                    <div className="flex p-12 mt-12">
-                        <div >
-                        
-                            <div className="font-archivo text-6xl">
-                                <h2 className="font-extrabold">Pantai</h2>
-                                <h2 className="font-extralight">Manggar.</h2>
-                            </div>
-                            <div className="pt-10 w-[500px]">
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, molestias! Quam, asperiores debitis eveniet quas nam aliquid expedita necessitatibus quo sed ea impedit, eum iusto deserunt quisquam velit temporibus tenetur.</p>
-                            </div>
-
-                            <div className="pt-20">
-                                <button type="button" class="py-2.5 px-5 mr-2 mb-2 text-xl font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-[#0353A4] focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Sewa tempat</button>
-                            </div>
-                        </div>
-                        <div className=" h-[400px] shadow-2xl mt-6 w-[4000px] ml-[50px] ">
-                            <img className="h-[400px] shadow-2xl " src="./assets/pantai-manggar.png" alt="" />
-                        </div>
-                    </div>
+                <div className="pt-20">
+                  <button
+                    type="button"
+                    class="py-2.5 px-5 mr-2 mb-2 text-xl font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-[#0353A4] focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                  >
+                    Sewa tempat
+                  </button>
                 </div>
-
-                <div className="bg-white  shadow-2xl rounded-3xl mx-auto w-[1200px] h-[600px]">
-                    <div className="flex p-12 mt-12">
-                        <div >
-
-                            <div className="font-archivo text-6xl">
-                                <h2 className="font-extrabold">Pantai</h2>
-                                <h2 className="font-extralight">Manggar.</h2>
-                            </div>
-                            <div className="pt-10 w-[500px]">
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, molestias! Quam, asperiores debitis eveniet quas nam aliquid expedita necessitatibus quo sed ea impedit, eum iusto deserunt quisquam velit temporibus tenetur.</p>
-                            </div>
-
-                            <div className="pt-20">
-                                <button type="button" class="py-2.5 px-5 mr-2 mb-2 text-xl font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-[#0353A4] focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Sewa tempat</button>
-                            </div>
-                        </div>
-                        <div className=" h-[400px] shadow-2xl mt-6 w-[4000px] ml-[50px] ">
-                            <img className="h-[400px] shadow-2xl " src="./assets/pantai-manggar.png" alt="" />
-                        </div>
-                    </div>
-                </div>
-
-                <div className="text-3xl my-auto ">
-                    <button className= "px-9 py-0 shadow-xl rounded-full bg-white text-gray-800 hover:bg-white">
-                        <Chevron direction={'right'} />
-                    </button>
-                </div>
+              </div>
+              <div className=" h-[400px] shadow-2xl mt-6 w-[4000px] ml-[50px] ">
+                <img
+                  className="h-[400px] shadow-2xl "
+                  src="./assets/pantai-manggar.png"
+                  alt=""
+                />
+              </div>
             </div>
+          </div>
 
-      
-            
-        </>
-    )
-}
+          <div className="bg-white  shadow-2xl rounded-3xl mx-auto w-[1200px] h-[600px]">
+            <div className="flex p-12 mt-12">
+              <div>
+                <div className="font-archivo text-6xl">
+                  <h2 className="font-extrabold">Waduk</h2>
+                  <h2 className="font-extralight">Manggar.</h2>
+                </div>
+                <div className="pt-10 w-[500px]">
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Cum, molestias! Quam, asperiores debitis eveniet quas nam
+                    aliquid expedita necessitatibus quo sed ea impedit, eum
+                    iusto deserunt quisquam velit temporibus tenetur.
+                  </p>
+                </div>
 
+                <div className="pt-20">
+                  <button
+                    type="button"
+                    class="py-2.5 px-5 mr-2 mb-2 text-xl font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-[#0353A4] focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                  >
+                    Sewa tempat
+                  </button>
+                </div>
+              </div>
+              <div className=" h-[400px] shadow-2xl mt-6 w-[4000px] ml-[50px] ">
+                <img
+                  className="h-[400px] shadow-2xl "
+                  src="./assets/krb.png"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+        </Slider>
+      </div>
+    </>
+  );
+};
 
 export default sliderAvailableFacilities;
--- src/pages/landingPages.jsx
+++ src/pages/landingPages.jsx
@@ -2,57 +2,70 @@ import NavBar from "../components/navBar";
 import SliderAvailableFacilities from "../components/sliderAvailableFacilities";
 import Chevron from "react-chevron";
 
-
-
 const landingPages = () => {
-    return (
-        <>  
-            <div className="bg-[#F0F0F0] h-full">
-                <NavBar />
+  return (
+    <>
+      <div className="bg-[#F0F0F0] h-full">
+        <NavBar />
 
-                <div className="h-60 md:h-[500px] lg:h-[700px] xl:h-[900px] bg-cover lg:bg-center xl:bg-center" style={{ backgroundImage: 'url("./assets/DashBoard.jpg")' }}>
-                    <div className="pl-[80px] pt-[258px] ">
-                        <div className="text-[#021d2e] text-7xl font-bold">
-                            <h2 >Selamat Datang Di <span className="text-[#003559]">FasilGO</span></h2>
-                        </div>
-                        <div className="text-[#0e2e50] font-semibold pt-4 font-roboto text-2xl">
-                            <p>"Melayani masyarakat yang ingin menyewa fasilitas pemerintah"</p>
-                            
-
-                        </div>
-
-                        <div className="pt-[44px] ">
-
-                            <button type="button" class="text-white font-semibold  hover:bg-[#4383c4] bg-blue-800  focus:outline-none   rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center">
-                                Sewa sekarang!
-                                <svg aria-hidden="true" class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                            </button>
-                        </div>
-                    </div>
-                </div>
-                
-                <div className="mt-56">
-                    <div>
-                        <h3 className="font-inter text-4xl text-center tracking-tighter font-extrabold ">Fasilitas Yang Tersedia</h3>
-                       
-                    </div>
-
-                    <div className="pt-20 ">
-                        <SliderAvailableFacilities />
-                       
-                    </div>
-                    
-                </div>
+        <div
+          className="h-60 md:h-[500px] lg:h-[700px] xl:h-[900px] bg-cover lg:bg-center xl:bg-center"
+          style={{ backgroundImage: 'url("./assets/DashBoard.jpg")' }}
+        >
+          <div className="pl-[80px] pt-[258px] ">
+            <div className="text-[#021d2e] text-7xl font-bold">
+              <h2>
+                Selamat Datang Di{" "}
+                <span className="text-[#003559]">FasilGO</span>
+              </h2>
             </div>
-            {/* banner */}
+            <div className="text-[#0e2e50] font-semibold pt-4 font-roboto text-2xl">
+              <p>
+                "Melayani masyarakat yang ingin menyewa fasilitas pemerintah"
+              </p>
+            </div>
 
+            <div className="pt-[44px] ">
+              <button
+                type="button"
+                class="text-white font-semibold  hover:bg-[#4383c4] bg-blue-800  focus:outline-none   rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center"
+              >
+                Sewa sekarang!
+                <svg
+                  aria-hidden="true"
+                  class="w-5 h-5 ml-2 -mr-1"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </button>
+            </div>
+          </div>
+        </div>
 
-          
-            {/* <img className="xl:h-[900px] xl:object-cover  w-full" src="./assets/DashBoard.jpg" alt="" /> */}
-        </>
-    )
+        <div className="mt-56">
+          <div>
+            <h3 className="font-inter text-4xl text-center tracking-tighter font-extrabold ">
+              Fasilitas Yang Tersedia
+            </h3>
+          </div>
 
-}
+          <div className="pt-20 ">
+            <SliderAvailableFacilities />
+          </div>
+        </div>
+      </div>
+      {/* banner */}
 
+      {/* <img className="xl:h-[900px] xl:object-cover  w-full" src="./assets/DashBoard.jpg" alt="" /> */}
+    </>
+  );
+};
 
 export default landingPages;
